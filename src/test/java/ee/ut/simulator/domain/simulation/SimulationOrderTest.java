f6360ec22ca7417b0a38f48b5c7f95b8e46ad472
madisliias <madisliias@gmail.com>
2012-09-09 11:40:20 +0000

    Use improved naming strategy for hibernate, add fake unit test

---
+++ src/test/java/ee/ut/simulator/domain/simulation/SimulationOrderTest.java
@@ -0,0 +1,13 @@
+package ee.ut.simulator.domain.simulation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SimulationOrderTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
