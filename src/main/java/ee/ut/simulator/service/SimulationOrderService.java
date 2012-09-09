dc10b146e5a5d244f9f5a4bc38a1b1bacac5c77b
madisliias <madisliias@gmail.com>
2012-09-09 20:38:10 +0000

    Unit test for json parsing

--- src/main/java/ee/ut/simulator/service/SimulationOrderService.java
+++ src/main/java/ee/ut/simulator/service/SimulationOrderService.java
@@ -1,4 +1,11 @@
 package ee.ut.simulator.service;
 
+import ee.ut.simulator.domain.simulation.parameter.DefaultParameters;
+
 public interface SimulationOrderService {
+    public void generateDefaultParameters();
+
+    DefaultParameters getDefaultParameters();
+
+    void setDefaultParameters(DefaultParameters defaultParameters);
 }
--- src/main/java/ee/ut/simulator/service/SimulationOrderServiceImpl.java
+++ src/main/java/ee/ut/simulator/service/SimulationOrderServiceImpl.java
@@ -8,10 +8,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class SimulationOrderServiceImpl implements SimulationOrderService {
-    public SimulationOrderServiceImpl() {
+    private DefaultParameters defaultParameters;
+
+    @Override
+    public DefaultParameters getDefaultParameters() {
+        return defaultParameters;
+    }
+
+    @Override
+    public void setDefaultParameters(DefaultParameters defaultParameters) {
+        this.defaultParameters = defaultParameters;
+    }
+
+    @Override
+    public void generateDefaultParameters() {
         try {
-            BufferedReader json = new BufferedReader(new FileReader("main/resources/parameters.json"));
+            BufferedReader json = new BufferedReader(new FileReader("src/main/resources/parameters.json"));
             DefaultParameters defaultParameters = new Gson().fromJson(json, DefaultParameters.class);
+            setDefaultParameters(defaultParameters);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
