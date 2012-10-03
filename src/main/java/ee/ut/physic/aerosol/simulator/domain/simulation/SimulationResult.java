77f27896f79ceba64e6e09fc4fae0df2d3a2a2eb
madisliias <madisliias@gmail.com>
2012-10-03 22:45:01 +0000

    Small code refactoring

--- src/main/java/ee/ut/physic/aerosol/simulator/domain/simulation/SimulationResult.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/domain/simulation/SimulationResult.java
@@ -1,34 +1,23 @@
 package ee.ut.physic.aerosol.simulator.domain.simulation;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class SimulationResult {
     private long id;
     private Date completionDate;
 
-	private SimulationProcess simulationProcess;
+    private SimulationProcess simulationProcess;
     private Set<SimulationResultValue> simulationResultValues = new HashSet<SimulationResultValue>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SIMU_RESULT_SEQ")
-    @SequenceGenerator(name = "SIMU_RESULT_SEQ", sequenceName="SIMU_RESULT_SEQ", allocationSize=1)
+    @SequenceGenerator(name = "SIMU_RESULT_SEQ", sequenceName = "SIMU_RESULT_SEQ", allocationSize = 1)
     public long getId() {
         return id;
     }
@@ -39,22 +28,22 @@ public class SimulationResult {
 
     @PrePersist
     protected void onCreate() {
-    	completionDate = new Date();
+        completionDate = new Date();
     }
-    
+
     @Temporal(TemporalType.TIMESTAMP)
     @DateTimeFormat(style = "M-")
     public Date getCompletionDate() {
-		return completionDate;
-	}
+        return completionDate;
+    }
 
-	public void setCompletionDate(Date completionDate) {
-		this.completionDate = completionDate;
-	}
+    public void setCompletionDate(Date completionDate) {
+        this.completionDate = completionDate;
+    }
 
     @ManyToOne
     public SimulationProcess getSimulationProcess() {
-    	return simulationProcess;
+        return simulationProcess;
     }
 
     public void setSimulationProcess(SimulationProcess simulationProcess) {
@@ -68,5 +57,9 @@ public class SimulationResult {
 
     public void setSimulationResultValues(Set<SimulationResultValue> simulationResultValues) {
         this.simulationResultValues = simulationResultValues;
+    }
+
+    public void addSimulationResultValue(SimulationResultValue simulationResultValue) {
+        getSimulationResultValues().add(simulationResultValue);
     }
 }
--- src/main/java/ee/ut/physic/aerosol/simulator/service/simulation/ResultFileParserServiceImpl.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/service/simulation/ResultFileParserServiceImpl.java
@@ -1,5 +1,13 @@
 package ee.ut.physic.aerosol.simulator.service.simulation;
 
+import ee.ut.physic.aerosol.simulator.Configuration;
+import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationProcess;
+import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationResult;
+import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationResultValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
@@ -8,65 +16,63 @@ import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
-import ee.ut.physic.aerosol.simulator.Configuration;
-import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationProcess;
-import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationResult;
-import ee.ut.physic.aerosol.simulator.domain.simulation.SimulationResultValue;
-
 @Service
 public class ResultFileParserServiceImpl implements ResultFileParserService {
-	final Logger log = LoggerFactory.getLogger(SimulationResultServiceImpl.class);
+    final Logger log = LoggerFactory.getLogger(ResultFileParserServiceImpl.class);
 
+    private SimulationResultValue createResultValue(SimulationResult result, String name, String value) {
+        SimulationResultValue resultValue = new SimulationResultValue();
+        resultValue.setName(name);
+        resultValue.setValue(value);
+        resultValue.setSimulationResult(result);
+        result.addSimulationResultValue(resultValue);
+        return resultValue;
+    }
+
+    private SimulationResult createSimulationResultFromLine(SimulationProcess process, String[] valueNames, String line) {
+        SimulationResult result = new SimulationResult();
+        result.setSimulationProcess(process);
+        int i = 0;
+        for (String value : line.split("\t")) {
+            createResultValue(result, valueNames[i], value);
+            i++;
+        }
+        return result;
+    }
+
+    // Parses result file into SimulationResults
     @Override
     public Set<SimulationResult> parseResultFile(SimulationProcess process) {
-		Set<SimulationResult> results = new HashSet<SimulationResult>();
-		try {
-			BufferedReader br = readResultFile();
-			String strLine;
-			String [] firstLine = new String[100];
-			while ((strLine = br.readLine()) != null)   {
-				// since I will be using names from first line in result, keeping this for future and skipping it
-				if(strLine.startsWith("time")) {
-					firstLine = strLine.split("\t");
-					continue;
-				}
-				SimulationResult simulationResult = new SimulationResult();	
-				simulationResult.setSimulationProcess(process);
-				
-				int count = 0;
-				for(String one : strLine.split("\t")) {
-					SimulationResultValue value = new SimulationResultValue();
-					value.setSimulationResult(simulationResult);
-					value.setName(firstLine[count]);
-					value.setValue(one);
-					simulationResult.getSimulationResultValues().add(value);
-					count ++;
-				}
-				results.add(simulationResult);
-			}
-		} catch(Exception e) {
-			log.error("error", e);
-		}
-		return results;
+        Set<SimulationResult> results = new HashSet<SimulationResult>();
+        try {
+            BufferedReader br = readResultFile();
+            // Read first line from buffer
+            String[] valueNames = br.readLine().split("\t");
+            String line;
+            // Read all other files from buffer
+            while ((line = br.readLine()) != null) {
+                SimulationResult simulationResult = createSimulationResultFromLine(process, valueNames, line);
+                results.add(simulationResult);
+            }
+            br.close();
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+        return results;
     }
 
     @Override
     public BufferedReader readResultFile() {
-    	Properties burstAppProperties = Configuration.getInstance().getBurstAppProperties();
+        Properties burstAppProperties = Configuration.getInstance().getBurstAppProperties();
         String path = burstAppProperties.getProperty("burstapp.path");
         String fullPath = path + burstAppProperties.getProperty("burstapp.outputFile");
-    	try {
-			FileInputStream stream = new FileInputStream(fullPath);
-			DataInputStream in = new DataInputStream(stream);
-			BufferedReader br = new BufferedReader(new InputStreamReader(in));
-			return br;
-		} catch (Exception e) {
-			log.error("error", e);
-		}
-    	return null;
+        try {
+            FileInputStream stream = new FileInputStream(fullPath);
+            DataInputStream in = new DataInputStream(stream);
+            return new BufferedReader(new InputStreamReader(in));
+        } catch (Exception e) {
+            log.error("error", e);
+        }
+        return null;
     }
 }
