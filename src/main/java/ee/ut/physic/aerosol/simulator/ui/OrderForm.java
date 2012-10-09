6035dc82bea1746bf7abbaf0937dc1baf56aceb1
s.neljandik <S.Neljandik@gmail.com>
2012-10-09 16:27:41 +0000

    UI ordering, it is still too high

--- src/main/java/ee/ut/physic/aerosol/simulator/ui/OrderForm.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/ui/OrderForm.java
@@ -30,6 +30,8 @@ public class OrderForm extends JPanel {
     public OrderForm(Collection<ParametersGroup> parametersGroups) {
         //LayoutManager layout = new BorderLayout();
         LayoutManager layout = new GridBagLayout();
+        LayoutManager layout2 = new GridBagLayout();
+        setLayout(layout2);
         JPanel paneel=new JPanel();
         JPanel paneel2=new JPanel();
         JPanel paneel3=new JPanel();
@@ -37,8 +39,11 @@ public class OrderForm extends JPanel {
         paneel2.setLayout(layout);
         paneel3.setLayout(layout);
         GridBagConstraints constraints = new GridBagConstraints();
+        GridBagConstraints constraints2 = new GridBagConstraints();
         constraints.weightx = 0.5;
         constraints.weighty = 0.5;
+        constraints2.weightx = 0.5;
+        constraints2.weighty = 0.5;
         constraints.fill = GridBagConstraints.HORIZONTAL;
 
         constraints.anchor = GridBagConstraints.NORTHWEST;
@@ -58,27 +63,87 @@ public class OrderForm extends JPanel {
                 j = 1;
             }
         }
-        JLabel commentLabel = new JLabel("Comment:");
+        constraints.gridx = 1;
+        JLabel commentLabel = new JLabel("Comment (Max 50 char.):");
         commentField = createCommentField();
         JLabel numberOfProcessesLabel = new JLabel("Number of simulations:");
         numberOfProcessesSpinner = createNumberOfProcessesSpinner();
         simulateButton = createSimulateButton();
+
+        JButton openM = new JButton("Open");
+        JButton openC = new JButton("Open");
+        JButton saveM = new JButton("Save");
+        JButton saveC = new JButton("Save");
+        JButton defaultM = new JButton("Default");
+        JButton defaultC = new JButton("Default");
+        JButton clearM = new JButton("Clear");
+        JButton clearC = new JButton("Clear");
+        JButton compare = new JButton("Compare");
+        JButton best = new JButton("Set best");
+        JButton impexp = new JButton("Import/Export");
+        JLabel minmax = new JLabel("Min Max Values:");
+        JLabel combo = new JLabel("Combobox Values:");
+
         constraints.weightx = 1.0;
         constraints.weighty = 1.0;
+
         constraints.gridx = 0;
         constraints.gridy = 0;
         paneel3.add(commentLabel, constraints);
         constraints.gridx = 1;
+        constraints.gridwidth= 4;
         paneel3.add(commentField, constraints);
-        constraints.gridx = 5;
+
+        constraints.gridwidth= 1;
+        constraints.gridy = 1;
+        constraints.gridx = 0;
         paneel3.add(numberOfProcessesLabel, constraints);
-        constraints.gridx = 6;
+        constraints.gridx = 1;
         paneel3.add(numberOfProcessesSpinner, constraints);
-        constraints.gridx = 7;
+
+        constraints.gridy = 2;
+        constraints.gridx = 0;
+        paneel3.add(minmax, constraints);
+        constraints.gridx = 1;
+        paneel3.add(openM, constraints);
+        constraints.gridx = 2;
+        paneel3.add(saveM, constraints);
+        constraints.gridx = 3;
+        paneel3.add(defaultM, constraints);
+        constraints.gridx = 4;
+        paneel3.add(clearM, constraints);
+
+        constraints.gridy = 3;
+        constraints.gridx=0;
+        paneel3.add(combo, constraints);
+        constraints.gridx = 1;
+        paneel3.add(openC, constraints);
+        constraints.gridx = 2;
+        paneel3.add(saveC, constraints);
+        constraints.gridx = 3;
+        paneel3.add(defaultC, constraints);
+        constraints.gridx = 4;
+        paneel3.add(clearC, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 6;
+        paneel3.add(compare, constraints);
+        constraints.gridx = 2;
+        paneel3.add(best, constraints);
+        constraints.gridx = 3;
+        paneel3.add(impexp, constraints);
+        constraints.gridx = 4;
         paneel3.add(simulateButton, constraints);
-        add(paneel);
-        add(paneel2);
-        add(paneel3);
+
+        constraints2.gridx=0;
+        constraints2.gridy=0;
+        add(paneel,constraints2);
+        constraints2.gridx=1;
+        add(paneel2,constraints2);
+        constraints2.gridx=0;
+        constraints2.gridy=1;
+        constraints2.gridwidth=2;
+        add(paneel3,constraints2);
     }
 
     private JTextField createCommentField() {
