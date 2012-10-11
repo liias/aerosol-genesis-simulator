3926110f88dca0e75758cfda5fad76f8ba1f0880
sellinjaanus <sellinjaanus@gmail.com>
2012-10-11 10:52:23 +0000

    [1942176] Fixed combobox values having too many decimal points

--- src/main/java/ee/ut/physic/aerosol/simulator/domain/simulation/parameter/ParameterDefinition.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/domain/simulation/parameter/ParameterDefinition.java
@@ -142,8 +142,10 @@ public class ParameterDefinition {
             float max = getMaximumValue();
             float step = getStep();
 
-            for (Float value = min; value < max; value += step) {
-                values.add(value.toString());
+            for (Float value = min; value < max; value += step) {        
+            	// little fix for adding floats, they can't be just added to eachother without overflowing
+            	Float savedValue = (float)(Math.round(value * 100))/100;
+                values.add(savedValue.toString());
             }
         }
         return values.toArray(new String[values.size()]);
