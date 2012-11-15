a8b6b206e454212b0c091aa6424e05071c7a44f9
sellinjaanus <sellinjaanus@gmail.com>
2012-11-15 19:45:46 +0000

    [2022264] - Do only one simulation, when no MAX values are defined.

--- src/main/java/ee/ut/physic/aerosol/simulator/service/simulation/ValidationService.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/service/simulation/ValidationService.java
@@ -12,15 +12,29 @@ import ee.ut.physic.aerosol.simulator.ui.OrderForm;
 @Service
 public class ValidationService {
 	final Logger log = LoggerFactory.getLogger(ValidationService.class);
-	
-    public void validateOrder(OrderForm orderForm) throws GeneralException {
-    	for(String title : orderForm.getAllParameterValues().keySet()) {
-    		Map<String, String> valueMap = orderForm.getAllParameterValues().get(title);
-    		for(String name : valueMap.keySet()) {  
-    			if(("forestMin".equals(name) || "freeAirMin".equals(name)) && valueMap.get(name).length() == 0) {
-    				throw new GeneralException("Fill all fields please (Minimum)");
-    			}
-    		}
-    	}
-    }
+
+	public void validateOrder(OrderForm orderForm) throws GeneralException {
+		for (String title : orderForm.getAllParameterValues().keySet()) {
+			Map<String, String> valueMap = orderForm.getAllParameterValues().get(title);
+			for (String name : valueMap.keySet()) {
+				if (("forestMin".equals(name) || "freeAirMin".equals(name)) && valueMap.get(name).length() == 0) {
+					throw new GeneralException("Fill all fields please (Minimum)");
+				}
+			}
+		}
+	}
+
+	public int validateSimulationCount(OrderForm orderForm, int initialCount) {
+		int count = 1;
+		for (String title : orderForm.getAllParameterValues().keySet()) {
+			Map<String, String> valueMap = orderForm.getAllParameterValues().get(title);
+			for (String name : valueMap.keySet()) {
+				if ("forestMax".equals(name) && valueMap.get(name).length() > 0 || 
+						"freeAirMax".equals(name) && valueMap.get(name).length() > 0) {
+					count = initialCount;
+				}
+			}
+		}
+		return count;
+	}
 }
