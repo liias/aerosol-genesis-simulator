4d7cbcd637dfec704a96130f003c231217e79686
madisliias <madisliias@gmail.com>
2012-11-16 21:41:50 +0000

    [2079387] Always use English locale, even if OS uses some other locale.

--- src/main/java/ee/ut/physic/aerosol/simulator/Launcher.java
+++ src/main/java/ee/ut/physic/aerosol/simulator/Launcher.java
@@ -3,9 +3,14 @@ package ee.ut.physic.aerosol.simulator;
 import ee.ut.physic.aerosol.simulator.ui.UIRunner;
 
 import javax.swing.*;
+import java.util.Locale;
 
 public class Launcher {
     public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        locale = Locale.ENGLISH;
+        Locale.setDefault(locale);
+
         // Instantiate configuration
         Configuration.getInstance();
         UIRunner uiRunner = new UIRunner();
